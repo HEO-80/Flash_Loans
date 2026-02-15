cf1f1afe258cae0ca524d4ce306ced58c729803b
HEO-80 <eovertigo@gmail.com>
2026-02-15 18:50:07 +0100

    finalizada Operación Financiera DeFi Profesional

--- 05_FlashLoanAgent/src/FlashLoanBot.sol
+++ 05_FlashLoanAgent/src/FlashLoanBot.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 // Importamos los contratos del "Banco" (Aave)
 import "aave-v3-core/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
